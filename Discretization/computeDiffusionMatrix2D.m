a37a0d1381fcf83ce88eb9e1a36acd26110ac277
Aina Frau-Pascual <ainafp@gmail.com>
2018-11-08 18:21:46 -0500

    function to compute the diffusion matrix for 2D and 3D cases: create mesh, define boundary conditions, and compute diffusion term

---
+++ Discretization/computeDiffusionMatrix2D.m
@@ -0,0 +1,22 @@
+function [meshstruct, D, M, RHSbc] = computeDiffusionMatrix2D(im, voxel_size)
+
+% Construct mesh structure
+Nx = size(im, 1);        % number of cells
+Lx = Nx * voxel_size(1); % domain length
+Ny = size(im, 2);        % number of cells
+Ly = Ny * voxel_size(2); % domain length
+Tx = size(im, 3);        % size tensor
+meshstruct = createMesh2D(Nx, Ny, Lx, Ly); 
+
+% Define the boundary condition:
+BC = createBC(meshstruct); % all Neumann boundary condition structure
+[Mbc, RHSbc] = boundaryCondition(BC); % boundary condition discretization
+
+% Define the transfer coefficients:
+[xvalue, yvalue] = extendBoundaryTensor2D_2v(im);
+D = FaceVariable(meshstruct, xvalue, yvalue, []);
+Dave = arithmeticMeanTensor(D); % convert a cell variable to face variable
+Mdiff = diffusionTermTensor2D_d(Dave);
+M = Mdiff + Mbc; % matrix of coefficient for central scheme
+
+figure(104); imagesc(Mbc); colormap jet; colorbar;
