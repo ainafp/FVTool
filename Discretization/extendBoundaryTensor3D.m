1a2a0bb414a728957375c68f6045758b3268fb5b
Aina Frau-Pascual <ainafp@gmail.com>
2018-11-08 18:23:47 -0500

    function to extend the boundaries with zeros when we are using tensors (we need more boundaries with tensors)

---
+++ Discretization/extendBoundaryTensor3D.m
@@ -0,0 +1,34 @@
+function [xvalue, yvalue, zvalue] = extendBoundaryTensor3D_2v(im)
+
+Nx = size(im, 1); % number of cells
+Ny = size(im, 2); % number of cells
+Nz = size(im, 3); % number of cells
+Tx = size(im, 4); % size tensor
+
+% Extend image
+xvalue = zeros(Nx + 2, Ny + 2, Nz + 2, Tx);
+xvalue(2:end-1, 2:end-1, 2:end-1, :) = im(:,:,:,:,1);
+xvalue(1,2:end-1,2:end-1,:) = im(1,:,:,:,1);
+xvalue(end,2:end-1,2:end-1,:) = im(end,:,:,:,1);
+xvalue(:,1,:,:) = xvalue(:,2,:,:);
+xvalue(:,end,:,:) = xvalue(:,end-1,:,:);
+xvalue(:,:,1,:) = xvalue(:,:,2,:);
+xvalue(:,:,end,:) = xvalue(:,:,end-1,:);
+
+yvalue = zeros(Nx + 2, Ny + 2, Nz + 2, Tx);
+yvalue(2:end-1, 2:end-1, 2:end-1, :) = im(:,:,:,:,2);
+yvalue(1,2:end-1,2:end-1,:) = im(1,:,:,:,2);
+yvalue(end,2:end-1,2:end-1,:) = im(end,:,:,:,2);
+yvalue(:,1,:,:) = yvalue(:,2,:,:);
+yvalue(:,end,:,:) = yvalue(:,end-1,:,:);
+yvalue(:,:,1,:) = yvalue(:,:,2,:);
+yvalue(:,:,end,:) = yvalue(:,:,end-1,:);
+
+zvalue = zeros(Nx + 2, Ny + 2, Nz + 2, Tx);
+zvalue(2:end-1, 2:end-1, 2:end-1, :) = im(:,:,:,:,3);
+zvalue(1,2:end-1,2:end-1,:) = im(1,:,:,:,3);
+zvalue(end,2:end-1,2:end-1,:) = im(end,:,:,:,3);
+zvalue(:,1,:,:) = zvalue(:,2,:,:);
+zvalue(:,end,:,:) = zvalue(:,end-1,:,:);
+zvalue(:,:,1,:) = zvalue(:,:,2,:);
+zvalue(:,:,end,:) = zvalue(:,:,end-1,:);
