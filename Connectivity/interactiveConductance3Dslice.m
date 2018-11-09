4e3ddd7ca19af624e4fa1a64815837e502f2ae0e
Aina Frau-Pascual <ainafp@gmail.com>
2018-11-08 19:19:07 -0500

    function to look at 2D conductance maps (a slice) of a voxel-to-voxel 3D analysis

---
+++ Connectivity/interactiveConductance3Dslice.m
@@ -0,0 +1,44 @@
+function interactiveConductance3Dslice(fa, mask, dim_size, potentials, mask_index, dim, slice)
+% Interactive conductance map
+
+% Conductance map
+if dim==3
+    fa2 = squeeze(fa(:,:,slice));
+    mask0 = squeeze(mask(:,:,slice));
+    dimx_size = dim_size([1,2]);
+elseif dim==2
+    fa2 = squeeze(fa(:,slice,:));
+    mask0 = squeeze(mask(:,slice,:));
+    dimx_size = dim_size([1,3]);
+else
+    fa2 = squeeze(fa(slice,:,:));
+    mask0 = squeeze(mask(slice,:,:));
+    dimx_size = dim_size([2,3]);
+end
+mnx = prod(dimx_size);
+
+% Interactive conductance map
+figure(107); image(fa2','CDataMapping','scaled'); colormap jet; colorbar;
+for i=1:100
+    figure(107);
+    [x, y] = ginput(1);
+    a1 = round(x);
+    b1 = round(y);
+    aux1 = sub2ind(dimx_size, a1, b1);
+    p1 = find(mask_index==aux1)
+    
+    c = zeros(dimx_size);
+    for p3=1:mnx
+        [a2, b2] = ind2sub(dimx_size, p3);
+        p2 = find(mask_index==p3);
+                        
+        if not(isempty(p1) || isempty(p2))
+            c(a2, b2) = abs(1 / ((potentials(a1,b1,p2) - potentials(a1,b1,p1)) ... 
+                             - (potentials(a2,b2,p2) - potentials(a2,b2,p1))));
+        end
+    end
+
+    % axial
+    figure(105); imagesc((c)'); caxis([0 0.005]); colormap(jet); colorbar;
+
+end
