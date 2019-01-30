1d4966238d7a6116bbd9f90bf37b2e5a8dfc0ff1
Aina Frau-Pascual <ainafp@gmail.com>
2019-01-30 18:27:29 -0500

    fixed computation of conductance here

--- Connectivity/interactiveConductance3Dslice.m
+++ Connectivity/interactiveConductance3Dslice.m
@@ -19,26 +19,42 @@ mnx = prod(dimx_size);
 
 % Interactive conductance map
 figure(107); image(fa2','CDataMapping','scaled'); colormap jet; colorbar;
+title('FA of the image')
+
+% Interactive conductance map
 for i=1:100
     figure(107);
     [x, y] = ginput(1);
-    a1 = round(x);
-    b1 = round(y);
+    if dim==3
+        a1 = round(x); b1 = round(y); c1 = slice;
+    elseif dim==2
+        a1 = round(x); b1 = slice; c1 = round(y);
+    else
+        a1 = slice; b1 = round(x); c1 = round(y);
+    end
     aux1 = sub2ind(dimx_size, a1, b1);
     p1 = find(mask_index==aux1);
     
     c = zeros(dimx_size);
     for p3=1:mnx
-        [a2, b2] = ind2sub(dimx_size, p3);
+        [a3, b3] = ind2sub(dimx_size, p3);
+        if dim==3
+            a2 = a3; b2 = b3; c2 = 1;
+        elseif dim==2
+            a2 = a3; b2 = 1; c2 = b3;
+        else
+            a2 = 1; b2 = a3; c2 = b3;
+        end
         p2 = find(mask_index==p3);
                         
         if not(isempty(p1) || isempty(p2))
-            c(a2, b2) = abs(1 / ((potentials(a1,b1,p2) - potentials(a1,b1,p1)) ... 
-                             - (potentials(a2,b2,p2) - potentials(a2,b2,p1))));
+            c(a3, b3) = abs(1 / ((potentials(a1,b1,c1,p2) - potentials(a1,b1,c1,p1)) ... 
+                             - (potentials(a2,b2,c2,p2) - potentials(a2,b2,c2,p1))));
         end
     end
 
     % axial
-    figure(105); imagesc((c)'); caxis([0 0.005]); colormap(jet); colorbar;
-
+    max_c = max(c(~isinf(c)));
+    figure(105); imagesc((c)'); caxis([0 max_c]); colormap(jet); colorbar;
+    title('Conductance map for a certain sink point')
 end
