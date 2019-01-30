58e155311141a47d01b02fcdf23b13cb931c7a1e
Aina Frau-Pascual <ainafp@gmail.com>
2019-01-30 11:46:05 -0500

    made plots nicer and added comments

--- Examples/Connectivity/example_2D_conductance_pointbypoint.m
+++ Examples/Connectivity/example_2D_conductance_pointbypoint.m
@@ -3,10 +3,14 @@
 % Author: Aina Frau-Pascual
 
 
-plot_potentials = false;
-use_phantom = true;
+% Set plot_potentials to true if you want to plot all potentials
+% WARNING: it is time consuming
+plot_potentials = false;   
 
-% Input image
+% Change use_phantom flag to use a different input image
+use_phantom = true;        
+
+% Input image: choose among the two (default: phantom)
 if use_phantom
     % input phantom in very low resolution
     im = phantom(10);
@@ -41,7 +45,8 @@ M = Mdiff + Mbc; % matrix of coefficient for central scheme
 Nx = Dave.domain.dims(1);
 Ny = Dave.domain.dims(2);
 G = reshape(1:(Nx+2)*(Ny+2), Nx+2, Ny+2);
-mnx = Nx*Ny;	mny = Nx*Ny;
+mnx = Nx*Ny;
+mny = Nx*Ny;
 rowx_index = reshape(G(2:Nx+1,2:Ny+1),mnx,1); % main diagonal x
 rowy_index = reshape(G(2:Nx+1,2:Ny+1),mny,1); % main diagonal y
 
@@ -51,22 +56,28 @@ for p1=rowx_index'
     for p2=rowy_index'
         if p2>p1
             RHSbc0 = RHSbc;
-            RHSbc0(p1) = 1; % define current i
+            RHSbc0(p1) = 1;  % define current i
             RHSbc0(p2) = -1; % define current j
             c = solvePDE(meshstruct, M, RHSbc0); % solve for the central scheme
             conductance(p1, p2) = abs(1 / (c.value(p1) - c.value(p2)));
             conductance(p2, p1) = conductance(p1, p2);
             if ~isnan(conductance(p1, p2)) && ~isinf(conductance(p1, p2)) && plot_potentials
-                figure(11); image(c.value(2:end-1,2:end-1), 'CDataMapping', 'scaled'); 
+                figure(100); image(c.value(2:end-1,2:end-1), 'CDataMapping', 'scaled'); 
                 colorbar; title('Potentials')
                 pause(.2)
             end
         end
     end
 end
+conductance(isnan(conductance)) = 0;
+
 
 % Plot results
-conductance(isnan(conductance)) = 0;
-figure(10); image(im,'CDataMapping','scaled'); colorbar; title('Image')
-figure(12); image(conductance(rowx_index,rowy_index), 'CDataMapping', 'scaled'); 
-colorbar; title('Conductance')
+figure; 
+set(gcf,'position', [300, 300, 1100, 400])
+subplot(1,2,1), image(im,'CDataMapping','scaled'); 
+colorbar; title('Original image'); xlabel('x'); ylabel('y');
+subplot(1,2,2), 
+image(conductance(rowx_index,rowy_index), 'CDataMapping', 'scaled'); 
+colorbar; title('Conductance matrix derived from original image');
+xlabel('voxel i'); ylabel('voxel s');
