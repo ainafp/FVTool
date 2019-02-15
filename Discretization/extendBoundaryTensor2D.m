1752127a0ba69ffdd6d3f66173a90f704073fca5
Aina Frau-Pascual <ainafp@gmail.com>
2019-02-15 11:11:46 -0500

    I am finally not modifying the 2D version

--- Discretization/extendBoundaryTensor2D.m
+++ Discretization/extendBoundaryTensor2D.m
@@ -7,19 +7,19 @@ function [xvalue, yvalue] = extendBoundaryTensor2D(im)
 
 Nx = size(im, 1); % number of cells
 Ny = size(im, 2); % number of cells
-Tx = 2;           % size tensor
+Tx = size(im, 3); % size tensor
 
 % Extend image
 xvalue = zeros(Nx + 2, Ny + 2, Tx);
-xvalue(2:end-1, 2:end-1, :) = im(:,:,1:2);
-xvalue(1,2:end-1,:) = im(1,:,1:2);
-xvalue(end,2:end-1,:) = im(end,:,1:2);
+xvalue(2:end-1, 2:end-1, :) = im(:,:,:,1);
+xvalue(1,2:end-1,:) = im(1,:,:,1);
+xvalue(end,2:end-1,:) = im(end,:,:,1);
 xvalue(:,1,:) = xvalue(:,2,:);
 xvalue(:,end,:) = xvalue(:,end-1,:);
 
 yvalue = zeros(Nx + 2, Ny + 2, Tx);
-yvalue(2:end-1, 2:end-1, :) = im(:,:,2:3);
-yvalue(1,2:end-1,:) = im(1,:,2:3);
-yvalue(end,2:end-1,:) = im(end,:,2:3);
+yvalue(2:end-1, 2:end-1, :) = im(:,:,:,2);
+yvalue(1,2:end-1,:) = im(1,:,:,2);
+yvalue(end,2:end-1,:) = im(end,:,:,2);
 yvalue(:,1,:) = yvalue(:,2,:);
 yvalue(:,end,:) = yvalue(:,end-1,:);
--- Examples/Connectivity/example_2D_tensor_fibercup.m
+++ Examples/Connectivity/example_2D_tensor_fibercup.m
@@ -10,8 +10,8 @@
 
 % Alternatively, you could generate your own tensors and load them as
 % im = (im_x, im_y, tensor_x, tensor_y)
-folder = 'your_folder';
-file_name = fullfile(folder, 'data.nii.gz.src.gz.012fz.dti.fib.gz'); 
+folder = '/autofs/space/galata_001/users/afrau/Data/Fibercup/';
+file_name = fullfile(folder, 'acq-averaged_b-1500.nii.gz.src.gz.012fz.dti.fib.gz'); 
 [fa0, md, im0, voxel_size] = read_fib(file_name);
 
 % Choose to use the whole image '0' (default), a medium size image '1' 
@@ -27,12 +27,15 @@ elseif image_size==1
 else
     ax = 1:size(im0,1); ay = 1:size(im0,2);
 end
-im = squeeze(im0(ax, ay, 2, [1, 2, 4]));
+im1 = squeeze(im0(ax, ay, 2, :));
+Nx = size(im1, 1); 
+Ny = size(im1, 2); 
+im = zeros(Nx, Ny, 2, 2);
+im(:, :, :, 1) = im1(:, :, 1:2);
+im(:, :, :, 2) = im1(:, :, [2, 4]);
 fa = fa0(ax, ay, 2);
 
 % Compute conductance and plot it interactively
-Nx = size(im, 1); 
-Ny = size(im, 2); 
 [meshstruct, D, M, RHSbc] = computeDiffusionMatrix2D(im, size(im));
 [potentials, mask_index] = computePotentials2D(D, meshstruct, M, RHSbc);
 interactiveConductance2D(fa, [Nx, Ny], potentials, mask_index);
